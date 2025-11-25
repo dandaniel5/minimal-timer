fc05efb3fac04697eecf3ee5ef5b0f15f1a0788b
[Shugaf433] <tg@Shugaf433>
2025-11-26 00:43:39 +0400

    Update formula to v1.0.3

--- Formula/timer.rb
+++ Formula/timer.rb
@@ -2,7 +2,7 @@ class Timer < Formula
   desc "Minimalist command-line timer with smart time parsing"
   homepage "https://github.com/dandaniel5/minimal-timer"
   url "https://github.com/dandaniel5/minimal-timer/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "402b8e7832b340a0871d2248807578cf445230867414f09be86ce5d293e22e2f"
+  sha256 "a1ced2407cd2c653644672000f10f3b64566159dfa8a87f4ce92812550311071"
   license "GPL-3.0"
 
   depends_on "python@3"
