2e0bbcb3e81c75e3a654694cae8ca7cb81cbab8a
[Shugaf433] <tg@Shugaf433>
2025-11-25 16:31:58 +0400

    Fix Homebrew checksum mismatch

--- Formula/timer.rb
+++ Formula/timer.rb
@@ -2,7 +2,7 @@ class Timer < Formula
   desc "Minimalist command-line timer with smart time parsing"
   homepage "https://github.com/dandaniel5/minimal-timer"
   url "https://github.com/dandaniel5/minimal-timer/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e0e979bc78fc13b39a673054e9453dca1df3a17eec0df092c080262e9a741b01"
+  sha256 "4f22d14de8ff4fb9e764d128f7cdca61f0686bdd9606fcf112ffa495b529784d"
   license "MIT"
 
   depends_on "python@3"
