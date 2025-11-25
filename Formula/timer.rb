b94632cf395e6e41c77fe248ca501f9d7a9b6ba1
[Shugaf433] <tg@Shugaf433>
2025-11-26 00:40:02 +0400

    Bump version to 1.0.3

--- Formula/timer.rb
+++ Formula/timer.rb
@@ -1,7 +1,7 @@
 class Timer < Formula
   desc "Minimalist command-line timer with smart time parsing"
   homepage "https://github.com/dandaniel5/minimal-timer"
-  url "https://github.com/dandaniel5/minimal-timer/archive/refs/tags/v1.0.2.tar.gz"
+  url "https://github.com/dandaniel5/minimal-timer/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "402b8e7832b340a0871d2248807578cf445230867414f09be86ce5d293e22e2f"
   license "GPL-3.0"
 
