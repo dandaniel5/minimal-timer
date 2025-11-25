e4185409a3637005777d67cf55affdf62dbbfa9d
[Shugaf433] <tg@Shugaf433>
2025-11-26 01:00:36 +0400

    Fix SHA256 for v1.0.4 (final)

--- Formula/timer.rb
+++ Formula/timer.rb
@@ -2,7 +2,7 @@ class Timer < Formula
   desc "Minimalist command-line timer with smart time parsing"
   homepage "https://github.com/dandaniel5/minimal-timer"
   url "https://github.com/dandaniel5/minimal-timer/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "a5090cadabe2aa3f2c032a160baedb5781737756e851a114284b82be8afef28d"
   license "GPL-3.0"
 
   depends_on "python@3"
